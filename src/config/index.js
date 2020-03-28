8927619380431b33aa0f8f054d5898af66c197f2
wstlima <wstlima@gmail.com>
2020-03-28 07:23:01 -0300

    Remove data private for api key

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 
 export default {
-  NAME: 'Well Lima',
-  PAGARME_CODE: '3645927080',
-  API_KEY_TEST: 'ak_test_r2a4lALE7INXx3cOyXgY39v62rpKoY',
-  ENCRYPTION_KEY_LIVE: 'ek_live_5BH3MHZtcnUeiI7U1p5WTtFdcG5uow',
-  ENCRYPTION_KEY_TEST: 'ek_test_DibDwXYVu5s4hZP68Ti1nq2uujX9ub',
+  NAME: '',
+  PAGARME_CODE: '',
+  API_KEY_TEST: '',
+  ENCRYPTION_KEY_LIVE: '',
+  ENCRYPTION_KEY_TEST: '',
 }
 	
